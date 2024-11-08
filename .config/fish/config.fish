74c1dbc76f5631a2cf1446b416db84dead44a578
fox <102655383+f0xb17@users.noreply.github.com>
2024-11-08 14:45:18 +0100

    Update config.fish
    Added some shortcuts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,6 +12,27 @@ alias lt='eza -aT --color=always --group-directories-first' # tree listing
 ### SETUP FZF ###
 fzf_configure_bindings --directory=\t
 
+### PACMAN AND YAY ###
+alias install='sudo pacman -S --needed --noconfirm'                     # Install package
+alias update='sudo pacman -Syu && yay --noconfirm && flatpak update'    # Update everything
+alias y='yay -S --noconfirm'                                            # Install package with Yay
+alias yc='yay -Scc && yay -Yc'                                          # Clear the Cache
+alias unlock='sudo rm /var/lib/pacman/db.lck'
+alias orphan='sudo pacman -Rns (pacman -Qtdq)'
+
+### GIT STUFF ###
+alias addup='git add -u'
+alias addall='git add .'
+alias branch='git branch'
+alias checkout='git checkout'
+alias clone='git clone'
+alias commit='git commit -m'
+alias fetch='git fetch'
+alias pull='git pull origin'
+alias push='git push origin'
+alias tag='git tag'
+alias newtag='git tag -a'
+
 ### SETUP ZOXIDE ###
 zoxide init fish | source
 
@@ -20,5 +41,5 @@ starship init fish | source
 
 ### REPLACE CAT WITH BAT ###
 if type -q bat
-    abbr --add -g cat 'bat'
+    abbr --add -g cat 'bat'v
 end
