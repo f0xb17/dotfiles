e90744460b636ebb95427073f87e9595cd670c48
fox <102655383+f0xb17@users.noreply.github.com>
2024-12-21 00:04:35 +0100

    Update scrolling.sh
    Fixed a typo in filepath

--- config/scripts/audio/scrolling.sh
+++ config/scripts/audio/scrolling.sh
@@ -4,7 +4,7 @@
 zscroll -l 30 \
         --delay 0.1 \
         --scroll-padding "  " \
-        --match-command "python ../..//Repos/dotfiles/config/scripts/audio/currently_playing.py" \
+        --match-command "python ../../Repos/dotfiles/config/scripts/audio/currently_playing.py" \
         --match-text "Playing" "--scroll 1" \
         --match-text "Paused" "--scroll 0" \
         --update-check true "python ../../Repos/dotfiles/config/scripts/audio/currently_playing.py" &
