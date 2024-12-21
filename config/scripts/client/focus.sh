328cbfe3624be877463830d8c9801f482747966f
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2024-12-21 15:21:57 +0100

    Removed useless stuff
    Removed stuff that isn't needed by the script.

--- config/scripts/client/focus.sh
+++ config/scripts/client/focus.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Usage: focus.sh <window_class>"
-    exit 1
-fi
-
 window_class=$1
 
 window_id=$(bspc query -N -n .window | while read -r node; do
@@ -13,10 +8,5 @@ window_id=$(bspc query -N -n .window | while read -r node; do
         break
     fi
 done)
-
-if [ -z "$window_id" ]; then
-    echo "No window found with class: $window_class"
-    exit 1
-fi
 
 bspc node $window_id -f
--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo pacman -S --needed --noconfirm python-psutil xorg-xprop wmctrl
+yay -S --needed zscroll-git
