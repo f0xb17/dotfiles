e5fed8479ab0c75f64960afa4c3c66235675a5a8
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2024-12-21 14:58:44 +0100

    Fixed some bugs
    If no qtile is installed, then of course no library of qtile can be called, so we first check whether libqtile is available at all.
    
    If not, then we don't need to run it at all.
    
    Also changed permission of focus.sh
    
    Added install.sh for later usage

--- config/scripts/client/focus.py
+++ config/scripts/client/focus.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
-import argparse, subprocess, os
-from libqtile.command.client import InteractiveCommandClient
+import argparse, subprocess, os, importlib.util
+def is_module_installed(module_name):
+    spec = importlib.util.find_spec(module_name)
+    return spec is not None
+
+if is_module_installed('libqtile'):
+    from libqtile.command.client import InteractiveCommandClient
+
+    def focus_client(client_name):
+        c = InteractiveCommandClient()
+        client_window = None
+        for window in c.windows():
+            wm_class = window['wm_class']
+            if wm_class and any(client_name in cls.lower() for cls in wm_class):
+                client_window = window
+                break
+
+        if client_window:
+            c.group[client_window['group']].toscreen()
 
 def get_window_manager_name():
     try:
@@ -35,7 +52,10 @@ def main():
     if get_window_manager_name() == "bspwm":
         subprocess.run([current_directory, client_name], capture_output=True, text=True, check=True)
     else:
-        focused = focus_client(client_name)
+        if is_module_installed('libqtile'):
+            focused = focus_client(client_name)
+        else:
+            return
     
 if __name__ == "__main__":
     main()
---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo pacman -S --needed --noconfirm python-psutil xorg-xprop wmctrl
